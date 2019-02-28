92c3dfb4dfd8bfdc4bc2ea4b6801445fb425ae7b
Khoa T. Nguyen <khoanguyen0791@gmail.com>
2019-02-28 00:13:55 -0600

    FR2 stable. FR1 unstable when processing ajax.

--- development/symfony/src/Controller/ProfileController.php
+++ development/symfony/src/Controller/ProfileController.php
@@ -7,6 +7,7 @@ use App\Entity\Occurrence\BehaviorOccurrence;
 use App\Entity\Occurrence\CumulativeTardinessOccurrence;
 use App\Entity\Occurrence\Occurrence;
 use App\Entity\Occurrence\OccurrenceType;
+use App\Entity\User\Info\Profile;
 use App\DataTransferObject\FileData;
 use App\Entity\Misc\Subject;
 use App\Entity\File\File;
@@ -25,268 +26,281 @@ use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 class ProfileController extends Controller
 {
-    /**
+  /**
      * @Route("/profile/{username}", name="profile")
      */
-    public function viewProfile(Request $request, User $user)
-    {
-        $isAdmin = $this->isGranted('admin');
+  public function viewProfile(Request $request, User $user)
+  {
+    $isAdmin = $this->isGranted('admin');
 
-        // Protect against non-admin user trying to view someone else's profile
-        if ($this->getUser() != $user && !$isAdmin) {
-            // Redirect to home instead of displaying a forbidden message
-            return $this->redirectToRoute('home');
-        }
-
-        return $this->render('role/mentor/profile.html.twig', array(
-            'user' => $user,
-            'isAdmin' => $isAdmin
-        ));
+    // Protect against non-admin user trying to view someone else's profile
+    if ($this->getUser() != $user && !$isAdmin) {
+      // Redirect to home instead of displaying a forbidden message
+      return $this->redirectToRoute('home');
     }
 
-    /**
+    return $this->render('role/mentor/profile.html.twig', array(
+      'user' => $user,
+      'isAdmin' => $isAdmin
+    ));
+  }
+
+  /**
      * @Route("/profile/{username}/ajaxDuplicatePrefName", name="ajax_duplicate_pref_name")
-     * Query the db for a mentor with matching username. If it exists, then return true. Else false.
+     * Query the db for a mentor with matching username. If it exists, then return NODUP. Else HASDUP.
      * @param Request $request
      * @return Response
      */
-    public function queryForDuplicatePreferredName(Request $request)
-    {
-      //need to query for preferred name of the same value
+  public function queryForDuplicatePreferredName(Request $request)
+  {
+    //need to query for preferred name of the same value
+    $payload = $request->getContent();
+    $criteria = array("preferredName" => $payload["preferredName"], "user" => $payload["id"]);
+    $preferredName = array("preferredName" => $payload["preferredName"]);
+    $userId = array("user" => $payload["id"]);
+
+    $preferredNames = $this->getDoctrine()->getRepository(Profile::class)->findBy($preferredName);
+    $exist = $this->getDoctrine()->getRepository(Profile::class)->findBy($criteria);
+    //$ids = $this->getDoctrine()->getRepository(Profile::class)->findBy($userId);
+
+    //if the name belongs to this user, then it's not a duplicate.
+    if (sizeof($exist) != 0 || sizeof($preferredNames) == 0) {
       return new Response("NODUP");
     }
-    /**
+    return new Response(implode(", ",$preferredName));
+  }
+  /**
      * @Route("/profile/{username}/showDetailedScore", name="show_detailed_score")
      * Query the db for a mentor with matching username. Return all occurences of that user.
      */
-    public function showScoreDetails(Request $request, User $user)
-    {
-        $pendingOccurrences = null;
-        $closedOccurrences = null;
+  public function showScoreDetails(Request $request, User $user)
+  {
+    $pendingOccurrences = null;
+    $closedOccurrences = null;
 
-        $requestedUser = null;
+    $requestedUser = null;
 
-        $userId = $request->query->get('user');
-        $foundMentor = false;
-        $user = $user;
+    $userId = $request->query->get('user');
+    $foundMentor = false;
+    $user = $user;
 
-        //if look for multiple mentor, then query everything.
-        $pendingOccurrences = $this->getDoctrine()->getRepository(Occurrence::class)
-            ->findPendingOccurrencesForDisplaying($user);
-        $closedOccurrences = $this->getDoctrine()->getRepository(Occurrence::class)
-            ->findClosedOccurrencesForDisplaying($user);
+    //if look for multiple mentor, then query everything.
+    $pendingOccurrences = $this->getDoctrine()->getRepository(Occurrence::class)
+      ->findPendingOccurrencesForDisplaying($user);
+    $closedOccurrences = $this->getDoctrine()->getRepository(Occurrence::class)
+      ->findClosedOccurrencesForDisplaying($user);
 
-        $occurrenceTypes = $this->getDoctrine()->getManager()->getRepository(OccurrenceType::class)->findAll();
-        $occurrenceTypes = array_map(function ($occurrenceType) {
-            return array(
-                'description' => $occurrenceType->getDescription(),
-                'defaultPoints' => $occurrenceType->getDefaultPoints()
-            );
-        }, $occurrenceTypes);
+    $occurrenceTypes = $this->getDoctrine()->getManager()->getRepository(OccurrenceType::class)->findAll();
+    $occurrenceTypes = array_map(function ($occurrenceType) {
+      return array(
+        'description' => $occurrenceType->getDescription(),
+        'defaultPoints' => $occurrenceType->getDefaultPoints()
+      );
+    }, $occurrenceTypes);
 
-        $penaltyManager = AttendancePenaltyPersistenceManager::loadModel($this->getDoctrine()->getManager());
-        $unjustifiedAbsencePenalties = $penaltyManager->getUnjustifiedAbsenceWithNoticePenalties();
-        $justifiedAbsencePenalties = $penaltyManager->getJustifiedAbsenceWithNoticePenalties();
+    $penaltyManager = AttendancePenaltyPersistenceManager::loadModel($this->getDoctrine()->getManager());
+    $unjustifiedAbsencePenalties = $penaltyManager->getUnjustifiedAbsenceWithNoticePenalties();
+    $justifiedAbsencePenalties = $penaltyManager->getJustifiedAbsenceWithNoticePenalties();
 
-        $unjustifiedAbsencePenalties = array_map(function ($penalty) {
-            return array(
-                'hoursBefore' => $penalty->getHoursBefore(),
-                'penaltyAmount' => $penalty->getPenaltyAmount()
-            );
-        }, $unjustifiedAbsencePenalties);
+    $unjustifiedAbsencePenalties = array_map(function ($penalty) {
+      return array(
+        'hoursBefore' => $penalty->getHoursBefore(),
+        'penaltyAmount' => $penalty->getPenaltyAmount()
+      );
+    }, $unjustifiedAbsencePenalties);
 
-        $justifiedAbsencePenalties = array_map(function ($penalty) {
-            return array(
-                'hoursBefore' => $penalty->getHoursBefore(),
-                'penaltyAmount' => $penalty->getPenaltyAmount()
-            );
-        }, $justifiedAbsencePenalties);
+    $justifiedAbsencePenalties = array_map(function ($penalty) {
+      return array(
+        'hoursBefore' => $penalty->getHoursBefore(),
+        'penaltyAmount' => $penalty->getPenaltyAmount()
+      );
+    }, $justifiedAbsencePenalties);
 
-        $noNoticePenaltyAmount = $penaltyManager->getAbsenceWithoutNoticePenalty();
-        if ($noNoticePenaltyAmount) {
-            $noNoticePenaltyAmount = $noNoticePenaltyAmount->getPenaltyAmount();
-        }
-
-        $noticeAmounts = [];
-
-        /** @var AbsenceOccurrence[] $absenceOccurrences */
-        $absenceOccurrences = $this->getDoctrine()->getRepository(AbsenceOccurrence::class)->findAll();
-
-        foreach ($absenceOccurrences as $absenceOccurrence) {
-            $noticeAmounts[$absenceOccurrence->getId()] = $absenceOccurrence->getHoursNotice();
-        }
-
-        return $this->render('role/mentor/detailed_score.html.twig', array(
-            'user' => $user,
-            'pendingOccurrences' => $pendingOccurrences,
-            'closedOccurrences' => $closedOccurrences,
-            'occurrenceTypes' => json_encode($occurrenceTypes),
-            'unjustifiedPenalties' => json_encode($unjustifiedAbsencePenalties),
-            'justifiedPenalties' => json_encode($justifiedAbsencePenalties),
-            'noNoticePenaltyAmount' => $noNoticePenaltyAmount,
-            'noticeAmounts' => json_encode($noticeAmounts),
-            'requestedUser' => $requestedUser
-        ));
+    $noNoticePenaltyAmount = $penaltyManager->getAbsenceWithoutNoticePenalty();
+    if ($noNoticePenaltyAmount) {
+      $noNoticePenaltyAmount = $noNoticePenaltyAmount->getPenaltyAmount();
     }
-    /**
+
+    $noticeAmounts = [];
+
+    /** @var AbsenceOccurrence[] $absenceOccurrences */
+    $absenceOccurrences = $this->getDoctrine()->getRepository(AbsenceOccurrence::class)->findAll();
+
+    foreach ($absenceOccurrences as $absenceOccurrence) {
+      $noticeAmounts[$absenceOccurrence->getId()] = $absenceOccurrence->getHoursNotice();
+    }
+
+    return $this->render('role/mentor/detailed_score.html.twig', array(
+      'user' => $user,
+      'pendingOccurrences' => $pendingOccurrences,
+      'closedOccurrences' => $closedOccurrences,
+      'occurrenceTypes' => json_encode($occurrenceTypes),
+      'unjustifiedPenalties' => json_encode($unjustifiedAbsencePenalties),
+      'justifiedPenalties' => json_encode($justifiedAbsencePenalties),
+      'noNoticePenaltyAmount' => $noNoticePenaltyAmount,
+      'noticeAmounts' => json_encode($noticeAmounts),
+      'requestedUser' => $requestedUser
+    ));
+  }
+  /**
      * @Route("/profile/{username}/edit", name="edit_profile")
      */
-    public function editProfile(Request $request, User $mentor)
-    {
-        // Protect against non-admin user trying to edit someone else's profile
-        $isAdmin = $this->isGranted('admin');
-        if ($this->getUser() != $mentor && !$isAdmin) {
-            // Redirect to home instead of displaying a forbidden message
-            return $this->redirectToRoute('home');
-        }
-
-        $form = $this->createForm(ProfileType::class,
-            ProfileFormData::createFromProfile($mentor->getProfile(), $this->getDoctrine()->getManager()),
-            array('is_admin' => $isAdmin));
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $mentor->getProfile()->updateFromFormData($form->getData(), $isAdmin);
-
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($mentor);
-            $entityManager->flush();
-            return $this->redirectToRoute('profile', array(
-                'username' => $mentor->getUsername()
-            ));
-        }
-
-        return $this->render('role/mentor/edit_profile.html.twig', array(
-            'user' => $mentor,
-            'form' => $form->createView(),
-            'isAdmin' => $isAdmin
-        ));
+  public function editProfile(Request $request, User $mentor)
+  {
+    // Protect against non-admin user trying to edit someone else's profile
+    $isAdmin = $this->isGranted('admin');
+    if ($this->getUser() != $mentor && !$isAdmin) {
+      // Redirect to home instead of displaying a forbidden message
+      return $this->redirectToRoute('home');
     }
 
-    /**
+    $form = $this->createForm(
+      ProfileType::class,
+      ProfileFormData::createFromProfile($mentor->getProfile(), $this->getDoctrine()->getManager()),
+      array('is_admin' => $isAdmin)
+    );
+
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+      $mentor->getProfile()->updateFromFormData($form->getData(), $isAdmin);
+
+      $entityManager = $this->getDoctrine()->getManager();
+      $entityManager->persist($mentor);
+      $entityManager->flush();
+      return $this->redirectToRoute('profile', array(
+        'username' => $mentor->getUsername()
+      ));
+    }
+
+    return $this->render('role/mentor/edit_profile.html.twig', array(
+      'user' => $mentor,
+      'form' => $form->createView(),
+      'isAdmin' => $isAdmin
+    ));
+  }
+
+  /**
      * @Route("/profile/{username}/save_image", name="save_profile_image")
      */
-    public function saveImageAction(Request $request)
-    {
-        if (!$request->isXmlHttpRequest()) {
-            throw new MethodNotAllowedException();
-        }
-        $file = new
-        FileData();
-        $file->file = $request->files->get('file');
-        $crop = $request->request->get('crop');
-        $canvas = $request->request->get('canvas');
-        $image = $request->request->get('image');
-
-        $this->get('logger')->debug($crop);
-
-        $em = $this->getDoctrine()->getManager();
-
-        $file = File::fromUploadData($file, $em, array(
-            'crop' => $crop,
-            'canvas' => $canvas,
-            'image' => $image
-        ));
-
-        $em->persist($file);
-
-        $user_id = $request->request->get('user');
-        $user = $this->getDoctrine()->getRepository(User::class)->find($user_id);
-
-        $isAdmin = $this->isGranted('admin');
-        $user->updateProfilePicture($file, $isAdmin);
-
-        $em->flush();
-
-        return new Response('success', 200);
+  public function saveImageAction(Request $request)
+  {
+    if (!$request->isXmlHttpRequest()) {
+      throw new MethodNotAllowedException();
     }
+    $file = new
+      FileData();
+    $file->file = $request->files->get('file');
+    $crop = $request->request->get('crop');
+    $canvas = $request->request->get('canvas');
+    $image = $request->request->get('image');
 
-    /**
+    $this->get('logger')->debug($crop);
+
+    $em = $this->getDoctrine()->getManager();
+
+    $file = File::fromUploadData($file, $em, array(
+      'crop' => $crop,
+      'canvas' => $canvas,
+      'image' => $image
+    ));
+
+    $em->persist($file);
+
+    $user_id = $request->request->get('user');
+    $user = $this->getDoctrine()->getRepository(User::class)->find($user_id);
+
+    $isAdmin = $this->isGranted('admin');
+    $user->updateProfilePicture($file, $isAdmin);
+
+    $em->flush();
+
+    return new Response('success', 200);
+  }
+
+  /**
      * @Route("/profile/{username}/update_image", name="update_profile_image")
      */
-    public function updateImageAction(Request $request)
-    {
-        if (!$request->isXmlHttpRequest()) {
-            throw new MethodNotAllowedException();
-        }
-        $user_id = $request->request->get('user');
-        $user = $this->getDoctrine()->getRepository(User::class)->find($user_id);
-        $file = $user->getProfile()->getProfilePictureModificationRequest()->getValue();
-
-        $crop = $request->request->get('crop');
-        $canvas = $request->request->get('canvas');
-        $image = $request->request->get('image');
-
-        $this->get('logger')->debug($crop);
-
-        $file->set('crop', $crop);
-        $file->set('canvas', $canvas);
-        $file->set('image', $image);
-
-        $em = $this->getDoctrine()->getManager();
-        $em->persist($file);
-
-        $em->flush();
-
-        return new Response('success', 200);
+  public function updateImageAction(Request $request)
+  {
+    if (!$request->isXmlHttpRequest()) {
+      throw new MethodNotAllowedException();
     }
+    $user_id = $request->request->get('user');
+    $user = $this->getDoctrine()->getRepository(User::class)->find($user_id);
+    $file = $user->getProfile()->getProfilePictureModificationRequest()->getValue();
 
-    /**
+    $crop = $request->request->get('crop');
+    $canvas = $request->request->get('canvas');
+    $image = $request->request->get('image');
+
+    $this->get('logger')->debug($crop);
+
+    $file->set('crop', $crop);
+    $file->set('canvas', $canvas);
+    $file->set('image', $image);
+
+    $em = $this->getDoctrine()->getManager();
+    $em->persist($file);
+
+    $em->flush();
+
+    return new Response('success', 200);
+  }
+
+  /**
      * @Route("/profile/{username}/image", name="profile_image")
      */
-    public function imageAction(User $user, ImageEditor $imageEditor)
-    {
-        $image = $user->getProfilePicture();
-        $image_name = $user->getUsername() . $image->get('extension');
+  public function imageAction(User $user, ImageEditor $imageEditor)
+  {
+    $image = $user->getProfilePicture();
+    $image_name = $user->getUsername() . $image->get('extension');
 
-        return $this->cropImage($imageEditor, $image_name, $image);
-    }
+    return $this->cropImage($imageEditor, $image_name, $image);
+  }
 
-    /**
+  /**
      * @Route("/profile/{username}/new_image", name="profile_requested_image")
      * @param User $user
      * @param ImageEditor $editor
      * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
-    public function requestedProfileImage(User $user, ImageEditor $editor)
-    {
-        $image = $user->getProfile()->getProfilePictureModificationRequest()->getValue();
-        $name = $user->getUsername() . '-request' . $image->get('extension');
+  public function requestedProfileImage(User $user, ImageEditor $editor)
+  {
+    $image = $user->getProfile()->getProfilePictureModificationRequest()->getValue();
+    $name = $user->getUsername() . '-request' . $image->get('extension');
 
-        return $this->cropImage($editor, $name, $image);
-    }
+    return $this->cropImage($editor, $name, $image);
+  }
 
-    /**
+  /**
      * @Route("/profile/{username}/origin_requested_image", name="origin_requested_image")
      * @param User $user
      * @param ImageEditor $editor
      * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
-    public function originRequestedImage(User $user, ImageEditor $editor)
-    {
-        $image = $user->getProfile()->getProfilePictureModificationRequest()->getValue();
-        $name = $user->getUsername() . '-origin.jpeg' . $image->get('extension');
-        $origin_image = $editor->getOriginImage($image);
-        return $this->file($origin_image, $name, ResponseHeaderBag::DISPOSITION_INLINE);
-    }
+  public function originRequestedImage(User $user, ImageEditor $editor)
+  {
+    $image = $user->getProfile()->getProfilePictureModificationRequest()->getValue();
+    $name = $user->getUsername() . '-origin.jpeg' . $image->get('extension');
+    $origin_image = $editor->getOriginImage($image);
+    return $this->file($origin_image, $name, ResponseHeaderBag::DISPOSITION_INLINE);
+  }
 
-    /**
+  /**
      * @param ImageEditor $imageEditor
      * @param string $image_name
      * @param File $image
      * @return \Symfony\Component\HttpFoundation\BinaryFileResponse
      */
-    private function cropImage(ImageEditor $imageEditor, string $image_name, File $image = null)
-    {
-        if (!$image) {
-            throw new ResourceNotFoundException();
-        }
-
-        $cropped_image = $imageEditor->getCroppedImage($image);
-
-        return $this->file($cropped_image, $image_name, ResponseHeaderBag::DISPOSITION_INLINE);
+  private function cropImage(ImageEditor $imageEditor, string $image_name, File $image = null)
+  {
+    if (!$image) {
+      throw new ResourceNotFoundException();
     }
 
+    $cropped_image = $imageEditor->getCroppedImage($image);
 
+    return $this->file($cropped_image, $image_name, ResponseHeaderBag::DISPOSITION_INLINE);
+  }
 }
