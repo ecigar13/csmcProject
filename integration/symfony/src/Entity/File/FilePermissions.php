22cacced7141054f4612014ea368cc557165cf9b
TingErik <erik75269@gmail.com>
2019-04-26 10:54:55 -0500

    Permission fixture and Update FilePermissions Entity

--- integration/symfony/src/Entity/File/FilePermissions.php
+++ integration/symfony/src/Entity/File/FilePermissions.php
@@ -3,7 +3,9 @@
 namespace App\Entity\File;
 
 use Doctrine\ORM\Mapping as ORM;
-
+use App\Entity\User\User;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
 /**
  * @ORM\Entity()
  */
@@ -31,4 +33,89 @@ class FilePermissions {
      * @ORM\Column(type="boolean", name="edit")
      */
     private $edit;
+
+    public function __construct(User $user, bool $view , bool $edit) {
+            $this->user = $user;
+            $this->view = $view;
+            $this->edit = $edit;
+        }
+    /**
+     * @return mixed
+     */
+    public function getVirtualFile()
+    {
+        return $this->virtualFile;
+    }
+
+    /**
+     * @param mixed $virtualFile
+     *
+     * @return self
+     */
+    public function setVirtualFile($virtualFile)
+    {
+        $this->virtualFile = $virtualFile;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * @param mixed $user
+     *
+     * @return self
+     */
+    public function setUser($user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getView()
+    {
+        return $this->view;
+    }
+
+    /**
+     * @param mixed $view
+     *
+     * @return self
+     */
+    public function setView($view)
+    {
+        $this->view = $view;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEdit()
+    {
+        return $this->edit;
+    }
+
+    /**
+     * @param mixed $edit
+     *
+     * @return self
+     */
+    public function setEdit($edit)
+    {
+        $this->edit = $edit;
+
+        return $this;
+    }
 }
